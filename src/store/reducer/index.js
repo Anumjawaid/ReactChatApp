5eb91cc57345d4e47f8cc05d108e810d6e94a8d9
Anumjawaid <anumjawaid99@gmail.com>
2020-11-09 07:21:12 +0500

    removed {}

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -3,8 +3,24 @@ const INITIL_STATE={
         name:"Anum",
         email:'anum45@.com'
 
-    }]
+    },
+    {
+        name:"husn",
+        email:'husn45@.com'
+    }
+]
 }
-export default (state=INITIL_STATE,ction)=>{
-    return state
+export default (state=INITIL_STATE,action)=>{
+    console.log("action",action)
+    switch(action.type){
+        case 'SETDATA':
+            return({
+                ...state,
+                users:[...state.users,action.data]
+            })
+        default:
+            return state    
+    }
+    console.log(state)
+   
 }
