35bf63799daeb2f2d253ade8660cf24483b8ce3e
Anumjawaid <anumjawaid99@gmail.com>
2020-11-09 07:20:44 +0500

    getderivedstate

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './style.css';
 import { connect  }from 'react-redux'
+import {set_data} from '../store/actions/index'
 
 class Homee extends React.Component{
     constructor(){
         super()
     }
+    static getDerivedStateFromProps(props,state){
+        console.log("coming from drs,",props)
+        return{
+            
+        }
+    }
     render(){
+        let user={name:"Umair",email:'umai892hk@com'}
         console.log("Propsss",this.props)
         return(
             <div>
                  <h1>Home of the chat App</h1>
+                  <button onClick={()=>this.props.set_data(user)}>SetData</button>
             </div>
         )
     }
@@ -19,4 +28,7 @@ const mik = (state)=>({
         users : state.users
         // name:'Abc'
 })
-export default connect(mik,null)(Homee);
+const mid = (dispatch) =>({
+    set_data :(data)=>dispatch(set_data(data))
+})
+export default connect(mik,mid)(Homee);
