a026295c794c3025e8db64c1547b6e2ef21d636e
Anumjawaid <anumjawaid99@gmail.com>
2020-11-09 07:21:42 +0500

    action

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import reducer from './reducer'
-import { createStore} from 'redux'
-// import thunk from 'redux-thunk';
+import { createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer)
+const store = createStore(reducer,applyMiddleware(thunk))
 
 export default store;
