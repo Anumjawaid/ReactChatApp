1508c225a3834c64dcd32c7a99aaa65a5da869ea
Anumjawaid <anumjawaid99@gmail.com>
2020-11-19 14:15:55 +0500

    facebook login function firebase

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,31 @@
-const set_data = (data) =>{
-    return (dispatch) =>{
-        console.log(data)
+import firebase from '../../config/index'
+// const set_data = (data) =>{
+//     return (dispatch) =>{
+//         console.log(data)
         
-            dispatch({type:"SETDATA",data:data})
+//             dispatch({type:"SETDATA",data:data})
             
         
+//     }
+// }
+const logf = () => {
+    return(dispatch) => {
+        var provider =new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInwithPopup(provider)
+        .then(function(result){
+            var token=result.credential.accessToken;
+            var user=result.user;
+
+            console.log("user==>",user)
+        }).catch(function(error){
+            var errorCode=error.code;
+            var errorMessage =error.message;
+            console.log(errorMessage)
+        });
+
+
     }
 }
-export {set_data }
+
+
+export {logf }
