af5a3b83e1599732bd4f183bf74d0366e8ac0686
Anumjawaid <anumjawaid99@gmail.com>
2020-11-21 22:15:21 +0500

    added states users after login

--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -1,10 +1,12 @@
 import React from 'react'   
+import { connect } from 'react-redux'
 
 class Chat extends React.Component{
     constructor(){
         super()
     }
     render(){
+        console.log("users==>",this.state)
         return(
             <div>
                 <h1>Chat</h1>
@@ -12,4 +14,7 @@ class Chat extends React.Component{
         )
     }
 }
-export default Chat;
+const mapstatetoprops = (state) => ({
+    current_user:state.current_user,
+})
+export default connect(mapstatetoprops,null)(Chat);
